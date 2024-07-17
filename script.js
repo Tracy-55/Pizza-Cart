d1e3ad89e8e2809679cee263399ffe9f437b4567
Tracy-55 <kellytracey0911@gmail.com>
2024-07-18 01:08:40 +0200

    git commit -m 'recent changes made'

--- script.js
+++ script.js
@@ -1,56 +1,51 @@
 function pizzaCart() {
     return {
-        cart: [],
-        totals: { small: 49.99, medium: 79.99, large: 149.99 },
-        totalCost: 0,
-        paymentAmount: 0,
-        isCheckingOut: false,
-        checkoutMessage: '',
         smallQuantity: 0,
         mediumQuantity: 0,
         largeQuantity: 0,
-        addToCart(size, quantity) {
-            let pizzaPrice = { small: 49.99, medium: 79.99, large: 149.99 }[size];
+        cart: [],
+        totalCost: 0,
+        isCheckingOut: false,
+        paymentAmount: 0,
+        checkoutMessage: '',
+        addToCart(size, quantity, price) {
             let existingItem = this.cart.find(item => item.size === size);
             if (existingItem) {
                 existingItem.quantity += quantity;
             } else {
-                this.cart.push({ size, price: pizzaPrice, quantity });
+                this.cart.push({ size, quantity, price });
             }
-            this.updateTotals();
+            this.calculateTotalCost();
         },
         removeFromCart(size) {
-            let index = this.cart.findIndex(item => item.size === size);
-            if (index !== -1) {
-                if (this.cart[index].quantity > 1) {
-                    this.cart[index].quantity--;
-                } else {
-                    this.cart.splice(index, 1);
+            let existingItem = this.cart.find(item => item.size === size);
+            if (existingItem) {
+                existingItem.quantity--;
+                if (existingItem.quantity <= 0) {
+                    this.cart = this.cart.filter(item => item.size !== size);
                 }
-                this.updateTotals();
             }
+            this.calculateTotalCost();
         },
-        updateTotals() {
-            this.totals.small = this.cart.filter(item => item.size === 'small').reduce((acc, item) => acc + item.quantity, 0);
-            this.totals.medium = this.cart.filter(item => item.size === 'medium').reduce((acc, item) => acc + item.quantity, 0);
-            this.totals.large = this.cart.filter(item => item.size === 'large').reduce((acc, item) => acc + item.quantity, 0);
-            this.totalCost = this.cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+        calculateTotalCost() {
+            this.totalCost = this.cart.reduce((total, item) => total + (item.price * item.quantity), 0);
         },
         checkout() {
             this.isCheckingOut = true;
         },
         processPayment() {
-            if (this.paymentAmount >= this.totalCost) {
-                this.checkoutMessage = 'Enjoy your pizzas!';
-                this.cart = [];
-                this.updateTotals();
+            if (this.paymentAmount < this.totalCost) {
+                this.checkoutMessage = 'Sorry that is not enough money! Enter the required amount.';
             } else {
-                this.checkoutMessage = 'Sorry - that is not enough money!';
+                this.checkoutMessage = `Enjoy your Pizza! Your change is R${(this.paymentAmount - this.totalCost).toFixed(2)}`;
+                this.resetCart();
             }
-            setTimeout(() => {
-                this.checkoutMessage = '';
-                this.isCheckingOut = false;
-            }, 3000);
+        },
+        resetCart() {
+            this.cart = [];
+            this.totalCost = 0;
+            this.isCheckingOut = false;
+            this.paymentAmount = 0;
         }
     }
 }
